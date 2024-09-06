1831c9acbdf4fbac82ff9bec34f59024d1587268
k4mmur <ernesto.camurca@startupdefense.io>
2024-09-06 14:46:33 +0100

    Loading state fixed

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,25 +1,32 @@
 "use client";
+
 import Navbar from "@/components/navbar";
 import SideBar from "@/components/sidebar";
 import { useAuth } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
   const { isLoaded, userId } = useAuth();
   const router = useRouter();
+  const [loading, setLoading] = useState(true); // Set loading initially
 
   useEffect(() => {
-    if (isLoaded && !userId) {
-      router.push("/sign-in"); // Redirect if not authenticated
+    if (isLoaded) {
+      if (!userId) {
+        router.push("/sign-in"); // Redirect if not authenticated
+      } else {
+        setLoading(false); // Set loading to false if authenticated
+      }
     }
   }, [isLoaded, userId, router]);
 
-  if (!isLoaded) {
-    // Optionally render a loading state while the auth status is being determined
+  // If auth status is loading or we are redirecting, show loading state
+  if (loading) {
     return <div>Loading...</div>;
   }
 
+  // Only render the content if the user is authenticated
   return (
     <div className="flex">
       <aside className="">
