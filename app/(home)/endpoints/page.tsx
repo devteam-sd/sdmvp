8c06fa7fb6dafa3a64d6c6b2d36cd750b9986728
Paulo Cavalcante <cesar.filho@startupdefense.io>
2024-09-07 14:34:17 +0100

    Improvements: - Build data-table

--- app/(home)/endpoints/page.tsx
+++ app/(home)/endpoints/page.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import { Payment, columns } from "@/app/(home)/endpoints/colums";
+import { DataTable } from "./data-table";
 
-const Endpoints = () => {
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "489e1d42",
+      amount: 125,
+      status: "processing",
+      email: "example@gmail.com",
+    },
+    // ...
+  ];
+}
+
+export default async function DemoPage() {
+  const data = await getData();
+
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-5xl font-bold">Endpoints</h1>
+    <div className="container mx-auto p-10">
+      <DataTable columns={columns} data={data} />
     </div>
   );
-};
-
-export default Endpoints;
+}
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-5xl font-bold">Dashboard</h1>
+    <div className="flex items-center justify-center">
+      <h1 className="text-5xl font-bold mt-10">Dashboard</h1>
     </div>
   );
 };
