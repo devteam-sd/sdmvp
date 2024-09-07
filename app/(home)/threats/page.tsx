024d537c4e46c06f11c2627f070e98725b466b38
Paulo Cavalcante <cesar.filho@startupdefense.io>
2024-09-07 14:34:17 +0100

    Improvements: - Build data-table

--- app/(home)/threats/page.tsx
+++ app/(home)/threats/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Threats = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-5xl font-bold">Threats</h1>
+    <div className="flex items-center justify-center">
+      <h1 className="text-5xl font-bold mt-10">Threats</h1>
     </div>
   );
 };
