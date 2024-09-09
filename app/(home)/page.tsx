4c96cbf29dd38009b13b4fd637823b593da2e403
Paulo Cavalcante <cesar.filho@startupdefense.io>
2024-09-09 13:36:06 +0100

    Improvements: - Initial Dashboard layout

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,8 +2,24 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className="flex items-center justify-center">
-      <h1 className="text-5xl font-bold mt-10">Dashboard</h1>
+    <div className="grid grid-cols-6 grid-rows-2 gap-10 p-20">
+      <div className="h-60 col-span-2 rounded-lg bg-blue-500 flex items-center justify-center">
+        <ul className="text-white text-2xl p-4">
+          <li># of Active Devices</li>
+        </ul>
+      </div>
+      <div className="col-span-2 col-start-3 rounded-lg bg-green-500 flex items-center justify-center">
+        <span className="text-white text-2xl">Threats</span>
+      </div>
+      <div className="col-span-2 col-start-5 rounded-lg bg-yellow-500 flex items-center justify-center">
+        <span className="text-white text-2xl">Unresolved</span>
+      </div>
+      <div className="col-span-3 row-start-2 rounded-lg bg-red-500 flex items-center justify-center">
+        <span className="text-white text-2xl">4</span>
+      </div>
+      <div className="col-span-3 col-start-4 row-start-2 rounded-lg bg-purple-500 flex items-center justify-center">
+        <span className="text-white text-2xl">5</span>
+      </div>
     </div>
   );
 };
