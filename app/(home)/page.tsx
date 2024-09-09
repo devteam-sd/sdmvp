17bff44bace1211b0a656c1cf864a7aecb57a824
Paulo Cavalcante <cesar.filho@startupdefense.io>
2024-09-09 21:47:14 +0100

    Improvements: - Dashboard layout with device and threats cards

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,27 +1,23 @@
+import { ShieldAlert, Computer } from "lucide-react";
+import DeviceCard from "@/components/devicecard";
 import React from "react";
+import ThreatsCard from "@/components/threatscard";
 
-const Dashboard = () => {
+export default function Dashboard() {
   return (
-    <div className="grid grid-cols-6 grid-rows-2 gap-10 p-20">
-      <div className="h-60 col-span-2 rounded-lg bg-blue-500 flex items-center justify-center">
-        <ul className="text-white text-2xl p-4">
-          <li># of Active Devices</li>
-        </ul>
-      </div>
-      <div className="col-span-2 col-start-3 rounded-lg bg-green-500 flex items-center justify-center">
-        <span className="text-white text-2xl">Threats</span>
-      </div>
-      <div className="col-span-2 col-start-5 rounded-lg bg-yellow-500 flex items-center justify-center">
-        <span className="text-white text-2xl">Unresolved</span>
-      </div>
-      <div className="col-span-3 row-start-2 rounded-lg bg-red-500 flex items-center justify-center">
-        <span className="text-white text-2xl">4</span>
-      </div>
-      <div className="col-span-3 col-start-4 row-start-2 rounded-lg bg-purple-500 flex items-center justify-center">
-        <span className="text-white text-2xl">5</span>
-      </div>
+    <div className="flex flex-row items-center justify-center space-x-6 p-10">
+      <DeviceCard
+        title="Devices"
+        icon={<Computer className="h-8 w-8 text-muted-foreground" />}
+        count={257}
+        active={221}
+      />
+      <ThreatsCard
+        title="Threats"
+        icon={<ShieldAlert className="h-8 w-8 text-muted-foreground" />}
+        count={335}
+        unresolved={23}
+      />
     </div>
   );
-};
-
-export default Dashboard;
+}
